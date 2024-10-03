ed21cdf935e3af114a66fdc4c734220695d8ab22
C.Manuel Hernández <cmanuelhm87@gmail.com>
2024-10-03 09:18:56 +0200

    LocalStorage

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -6,24 +6,40 @@ import { Gif, SearchResponse } from '../interfaces/gifs.interfaces';
 export class GifsService {
   public gifList: Gif[] = [];
 
-  private _tagHistory: string[] = [];
+  private _tagsHistory: string[] = [];
   private apiKey: string = 'YOAWolhvjRm1rBgFFYpQbsFzr8F4prsM';
   private serviceUrl: string = 'https://api.giphy.com/v1/gifs';
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+    this.loadLocalStorage();
+  }
 
   get tagsHistory() {
-    return [...this._tagHistory];
+    return [...this._tagsHistory];
   }
 
   private organizeHistory(tag: string) {
     tag = tag.toLowerCase();
 
-    if (this._tagHistory.includes(tag)) {
-      this._tagHistory = this._tagHistory.filter((oldTag) => oldTag !== tag);
+    if (this._tagsHistory.includes(tag)) {
+      this._tagsHistory = this._tagsHistory.filter((oldTag) => oldTag !== tag);
     }
-    this._tagHistory.unshift(tag);
-    this._tagHistory = this.tagsHistory.splice(0, 10);
+    this._tagsHistory.unshift(tag);
+    this._tagsHistory = this.tagsHistory.splice(0, 10);
+    this.saveLocalStorage();
+  }
+
+  private saveLocalStorage(): void {
+    localStorage.setItem('history', JSON.stringify(this._tagsHistory));
+  }
+
+  private loadLocalStorage(): void {
+    if (!localStorage.getItem('history')) return;
+
+    this._tagsHistory = JSON.parse(localStorage.getItem('history')!);
+
+    if (this._tagsHistory.length === 0) return;
+    this.searchTag(this._tagsHistory[0]);
   }
 
   async searchTag(tag: string): Promise<void> {
