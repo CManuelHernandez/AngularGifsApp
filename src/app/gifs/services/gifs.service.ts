a45c509bfd79958a1d90abcaa987874f5899f6ba
C.Manuel Hernández <cmanuelhm87@gmail.com>
2024-10-03 08:53:17 +0200

    Giphy realizar una petición HTTP

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,9 +1,13 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({ providedIn: 'root' })
 export class GifsService {
   private _tagHistory: string[] = [];
-  constructor() {}
+  private apiKey: string = 'YOAWolhvjRm1rBgFFYpQbsFzr8F4prsM';
+  private serviceUrl: string = 'https://api.giphy.com/v1/gifs';
+
+  constructor(private http: HttpClient) {}
 
   get tagsHistory() {
     return [...this._tagHistory];
@@ -19,8 +23,17 @@ export class GifsService {
     this._tagHistory = this.tagsHistory.splice(0, 10);
   }
 
-  searchTag(tag: string): void {
+  async searchTag(tag: string): Promise<void> {
     if (tag.length === 0) return;
     this.organizeHistory(tag);
+
+    const params = new HttpParams()
+      .set('api_key', this.apiKey)
+      .set('limit', 10)
+      .set('q', tag);
+
+    this.http.get(`${this.serviceUrl}/search`, { params }).subscribe((resp) => {
+      console.log(resp);
+    });
   }
 }
